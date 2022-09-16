19994ee81c3c82930e0bf80601fc506e17d959b8
Анастасия <nastenka_slastenka2022@mail.ru>
2022-09-16 22:20:40 +0800

    add Array Formating code c#

---
+++ Task/ArrayFormating/Program.cs
@@ -0,0 +1,43 @@
+﻿string[] line = new string[] { "hello","2","world","=-)" };
+
+
+int numberWordsWithThreeOrLessCharacters(string[] line){
+    int count = 0;
+      for (int i = 0; i < line.Length; i++)
+    {
+        if (line[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int lengthFormatingArray =  numberWordsWithThreeOrLessCharacters(line);
+
+string[] ArrayFormation(string[] line, int LengthArrayWord)
+{
+    string[] formationLine = new string[LengthArrayWord];
+    int temp = 0;
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (line[i].Length <= 3)
+        {
+            formationLine[temp] = line[i];
+            temp++;
+        }
+    }
+    return formationLine;
+}
+
+string[] resultFormatingArray = ArrayFormation(line, lengthFormatingArray);
+
+void printArray(string[] resultFormArr)
+{
+    foreach (string word in resultFormArr)
+    {
+        Console.Write($"[{word}] ");
+    }
+}
+
+printArray(resultFormatingArray);
